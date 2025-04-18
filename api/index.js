56d7f202ab1b3a05afd9322c4fe010944fe4407d
JessCP25 <jpayanoc25@gmail.com>
2025-04-18 15:54:18 -0500

    Feat: gestion avanzada de errores

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const user = require('./components/user/network');
 const auth = require('./components/auth/network');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
+const errors = require('../network/errors');
 
 const app = express();
 
@@ -15,6 +16,7 @@ const swaggerDoc = require('./swagger.json');
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+app.use(errors);
 
 app.listen(config.api.port, () => {
   console.log('Escuchando en el puerto ', config.api.port)
