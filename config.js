a423f9b207994f98eb7db10386dd0635331e9a03
JessCP25 <jpayanoc25@gmail.com>
2025-05-20 15:04:07 -0500

    Feat: Almacenamiento de datos con mysql

--- config.js
+++ config.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
+
 module.exports = {
   api: {
     port: process.env.API_PORT || 3000
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'secret'
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || '',
+    user: process.env.MYSQL_USER || '',
+    password: process.env.MYSQL_PASS || '',
+    database: process.env.MYSQL_DB || '',
+    port: 3306
   }
 }
---
+++ store/mysql.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql2');
+
+const config = require('../config');
+
+const dbconfig = {
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: config.mysql.database,
+  port: config.mysql.port
+}
+
+let connection;
+
+function handleCon(){
+  connection = mysql.createConnection(dbconfig);
+
+  connection.connect((err) => {
+    if(err){
+      console.error('[db err]', err);
+      setTimeout(handleCon, 2000);
+    }else{
+      console.log('BD Connected!');
+    }
+  });
+
+  connection.on('error', err => {
+    console.error('[db err]', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+      handleCon();
+    }else{
+      throw err;
+    }
+  })
+}
+
+handleCon();
+
+function list(tabla){
+  return new Promise((resolve, reject)=>{
+    connection.query(`SELECT * FROM ${tabla}`, (err,data) => {
+      if(err) return reject(err);
+      resolve(data);
+    })
+  })
+}
+
+module.exports = {
+  list
+}
